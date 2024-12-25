aae66f832dd445ece2d8594e0445e45c7d79c28f
liz771124 <liz771124@yahoo.com.tw>
2024-12-26 02:45:56 +0800

    add: 新專案收納

---
+++ furniture-replacement/vite.config.js
@@ -0,0 +1,41 @@
+// vite.config.js
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+
+export default defineConfig({
+  base: "./", // 使用相對路徑
+  plugins: [
+    vue({
+      template: {
+        transformAssetUrls: {
+          includeAbsolute: false,
+        },
+      },
+    }),
+  ],
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    sourcemap: false, // 關閉 sourcemap
+    cssCodeSplit: false, // 禁用 CSS 代碼分割
+    rollupOptions: {
+      output: {
+        // 確保生成的資源使用相對路徑
+        assetFileNames: (assetInfo) => {
+          const info = assetInfo.name.split(".");
+          let extType = info[info.length - 1];
+          if (/\.(png|jpe?g|gif|svg|webp|ico)(\?.*)?$/.test(assetInfo.name)) {
+            extType = "imgs";
+          }
+          return `assets/${extType}/[name]-[hash][extname]`;
+        },
+        chunkFileNames: "assets/js/[name]-[hash].js",
+        entryFileNames: "assets/js/[name]-[hash].js",
+      },
+    },
+    // 配置 Vite 不添加 crossorigin
+    target: "esnext",
+    modulePreload: false,
+  },
+});
