a423df40271476f85fe11d528ee9931a22d18b6b
liz771124 <liz771124@yahoo.com.tw>
2024-12-30 00:10:36 +0800

    add: 新專案野餐測試

--- 2025-picnic-party/src/views/Gift.vue
+++ 2025-picnic-party/src/views/Gift.vue
@@ -1,4 +1,25 @@
+<script setup>
+  import { onMounted } from "vue";
+  import { Tooltip, initTWE } from "tw-elements";
+
+  import BG from '../assets/img/bg_all.gif'
+  onMounted(() => {
+    initTWE({ Tooltip });
+  });
+</script>
+
 <template>
-    <h1>Gift</h1>
-  </template>
-  
+  <div class="mt-16 flex justify-center" :style="{background: `url(${BG})`}">
+    <p class="text-lg">
+      Hover the link to see the
+      <a
+        href="#"
+        class="text-primary transition duration-150 ease-in-out hover:text-primary-600 focus:text-primary-600 active:text-primary-700 dark:text-primary-400 dark:hover:text-primary-500 dark:focus:text-primary-500 dark:active:text-primary-600"
+        data-twe-toggle="tooltip"
+        title="Hi! I'm tooltip"
+        >tooltip</a
+      >
+      <img src="../assets/img/date.png">
+    </p>
+  </div>
+</template>
--- 2025-picnic-party/tailwind.config.js
+++ 2025-picnic-party/tailwind.config.js
@@ -1,10 +1,13 @@
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    "./index.html", // 檢查 index.html 檔案
-    "./src/**/*.{js,ts,jsx,tsx,vue}", // 檢查 src 目錄下所有可能的檔案
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./node_modules/tw-elements/js/**/*.js"
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
-};
+  darkMode: "class",
+  plugins: [require("tw-elements/plugin.cjs")]
+}
