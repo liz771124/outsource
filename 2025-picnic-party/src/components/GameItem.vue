11791c87ababf963f7a07790bd2eaa355d7df2ea
liz771124 <liz771124@yahoo.com.tw>
2025-02-04 18:53:59 +0800

    fix: 野餐切版更新

--- 2025-picnic-party/src/components/GameItem.vue
+++ 2025-picnic-party/src/components/GameItem.vue
@@ -25,7 +25,7 @@
     @mouseenter="isHovered = index"
     @mouseleave="isHovered = null"
   >
-    <div class="overflow-hidden">
+    <div class="overflow-hidden bg-gray-200">
       <span
         class="group:hover:translate-x-0 absolute inset-0 z-10 h-full overflow-hidden bg-white px-3 pb-3 pt-10 transition-opacity duration-300"
         :class="isHovered === index ? 'opacity-100' : 'opacity-0'"
@@ -33,14 +33,14 @@
         <div v-html="item.description" class="h-full overflow-auto"></div>
       </span>
       <img
-        class="aspect-6/4 h-[250px] w-full rounded-tl-2xl object-cover transition-opacity duration-300"
+        class="aspect-1/1 w-full rounded-tl-2xl object-contain transition-opacity duration-300"
         :class="isHovered === index ? 'opacity-0' : 'opacity-100'"
         :src="`${imagePath}${item.id}`"
         alt=""
       />
     </div>
     <span
-      :class="`absolute -end-3 -top-3 z-10 inline-block w-full max-w-[330px] rounded-tr-xl px-6 py-1.5 text-[16px] font-bold tracking-wide text-white ${className}`"
+      :class="`absolute -top-3 end-3 z-10 inline-block w-full max-w-[330px] rounded-tr-xl px-6 py-1.5 text-[16px] font-bold tracking-wide text-white ${className}`"
     >
       {{ item.name }}
     </span>
