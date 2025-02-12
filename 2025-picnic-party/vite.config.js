395ad67f6597f4f99a4df73b7b3f9d6f6cc7df48
liz771124 <liz771124@yahoo.com.tw>
2025-02-13 01:19:37 +0800

    fix: 改掉路由的 # 生成 config base 網址設定

--- 2025-picnic-party/vite.config.js
+++ 2025-picnic-party/vite.config.js
@@ -8,8 +8,8 @@ import Components from 'unplugin-vue-components/vite'
 export default defineConfig({
   base:
     process.env.NODE_ENV === 'production'
-      ? './'
-      : '/outsource/2025-picnic-party/dist/',
+      ? './outsource/2025-picnic-party/dist/'
+      : '/',
   plugins: [vue(), Pages(), Components({ dirs: ['src/components'] })],
   resolve: {
     alias: {
